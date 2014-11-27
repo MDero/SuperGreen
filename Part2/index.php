1f291283027bedecedc12901baf2472fa4b86ff1
MDero <maxime.derobillard@gmail.com>
2014-11-27 19:46:02 +0100

    Test slide-master

--- Part2/index.php
+++ Part2/index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title> Upload Images to the server </title>
+    <head>
+        <title> Upload Images to the server </title>
 
-<!-------Including jQuery from Google ------>
-<script src="jquery.min.js"></script>
+        <!-------Including jQuery from Google ------>
+        <script src="jquery.min.js"></script>
 
-<!------- Including CSS File ------>
-<link rel="stylesheet" type="text/css" href="style.css">
+        <!------- Including CSS File ------>
+        <link rel="stylesheet" type="text/css" href="style.css">
 
-<body>
-		<div id="formdiv">
-		
-			<form enctype="multipart/form-data" action="" method="post">
-			<h1>Image Upload</h1>
-			Only JPG, JPEG,PNG, and GIF can be Uploaded. 
-			
-			<br><br>
-			
-			<input id="file" name="file[]" type="file" multiple="" />
-			
-			<br><br>
-			
-			<input type="submit" value="Upload File" name="submit" id="upload" class="upload"/>
-			
-			
-			<br><br>
-			
-			<?php include "upload.php"; ?>
+    <body>
+        <div id="formdiv">
 
-			</form>
+            <form enctype="multipart/form-data" action="" method="post">
+                <h1>Image Upload</h1>
+                Only JPG, JPEG,PNG, and GIF can be Uploaded. 
 
-		</div>
-</body>
+                <br><br>
+
+                <input id="file" name="file[]" type="file" multiple="" />
+
+                <br><br>
+
+                <input type="submit" value="Upload File" name="submit" id="upload" class="upload"/>
+
+
+                <br><br>
+
+                <?php include "upload.php"; ?>
+
+            </form>
+
+        </div>
+    </body>
 
 </html>
