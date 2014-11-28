1b2e410bff226a6d0bb7e891e670917dbab0be48
Valentin SABATET <sabatet.valentin@gmail.com>
2014-11-28 01:03:10 +0100

    finalisation du slideshow + test phone

--- slideshow/slider.js
+++ slideshow/slider.js
@@ -2,12 +2,73 @@ function addTimeLine() {
 	$length = $(".slider li").length;
 	$plan = $("#plan");
 	
-	$plan.append('<li class="activeButton"></li>');
+	$plan.append('<li class="activeButton"><a></a></li>');
 	for($i=1; $i<$length; ++$i)
-		$plan.append('<li class="otherButton"></li>');
+		$plan.append('<li><a></a></li>');
 }
-			
-$(function(){
+
+function goTo(indexLi) {
+	$currentSlide = $("#plan li").eq(indexLi);
+	$capt = $currentSlide.find(".picCaption");
+	($capt.html() != "") ? $capt.show() : $capt.hide();
+	
+	$("#timeline").html((currentIndex+1) + "/" + $lastChild);
+	$("#plan li").removeClass("activeButton");
+	$currentSlide.addClass("activeButton");
+}
+
+function prevDiapo() {
+	var transi = Math.round(Math.random() * (2 - 1) + 1);
+	//$formerDiapo = $(".slider li").eq(currentIndex).hide();
+	currentIndex = (currentIndex == 0) ? $(".slider li").length-1 : currentIndex-1;
+	
+	/*switch(transi) {
+		case 1 :
+				$("#next, #prev").unbind("click");
+				$(".slider li").fadeIn(600, function() {
+					$("#next").click(nextDiapo);
+					$("#prev").click(prevDiapo);
+				});
+				break;
+		case 2 : */
+				$(".slider").dequeue().animate({marginLeft:0},1000,function(){  
+					$(this).css({marginLeft:-900}).find("li:first").before($(this).find("li:last"));  
+				});
+			/*	break;
+		default : break;
+	}*/
+	
+	goTo(currentIndex);
+}
+
+function nextDiapo() {
+	var transi = Math.round(Math.random() * (2 - 1) + 1);
+	//$formerDiapo = $(".slider li").eq(currentIndex).hide();
+	currentIndex = (currentIndex == ($(".slider li").length-1)) ? 0 : currentIndex+1;
+	
+	/*switch(transi) {
+		case 1 :
+				$("#next, #prev").unbind("click");
+				$(".slider li").eq(currentIndex).fadeIn(600, function() {
+					$("#next").click(nextDiapo);
+					$("#prev").click(prevDiapo);
+				});
+				break;
+		case 2 : */
+				$(".slider").dequeue().animate({marginLeft:-1800},1000,function(){  
+					$(this).css({marginLeft:-900}).find("li:last").after($(this).find("li:first"));  
+				});				
+			/*	break;
+		default : break;
+	}*/
+	
+	goTo(currentIndex);
+}
+
+$(function() {
+	$(".containerSlide").bind("mouseenter", function() { if($("#play").is(":hidden")) { clearInterval(timer) }})
+						.bind("mouseleave", function() { if($("#play").is(":hidden")) { timer = setInterval(nextDiapo, slideInterval) }});
+
 	$("#pause").click(function() {
 		$(this).dequeue();
 		clearInterval(timer);
@@ -19,30 +80,23 @@ $(function(){
 		clearInterval(timer);
 		timer = setInterval(nextDiapo, slideInterval);
 		$("#pause").show();
-		$("#play").hide()
+		$("#play").hide();
 	});
-
-	$("#next").click(function() {
-		var taille = -1*$(".slider li").width();
-		$(".slider").animate({marginLeft:-1800},1000,function(){  
-			$(this).css({marginLeft:-900}).find(".slider li:last").after($(this).find(".slider li:first"));  
-		});
+	
+	$("#plan li").live("click", function() {
+		var index = $("#plan li").index($(this));
+		$currentSlide = $(".slider li").eq(index);
+		$formerSlide = $(".slider li").eq(currentIndex);
+		goTo(index);
+		
+		$formerSlide.hide();
+		$currentSlide.show();
+		
+		currentIndex = index; 
+		$("#pause").trigger("click");
 	});
-
-	$('#prev').click(function(){
-		var taille = -1*$(".slider li").width();
-		 $(".slider").animate({marginLeft:0},1000,function(){  
-			$(this).css({marginLeft:-900}).find(".slider li:first").before($(this).find(".slider li:last"));  
-		}) 
-	});
+		
+	
+	$("#next").click(nextDiapo);
+	$('#prev').click(prevDiapo);
 });
-
-
-
-
-
-
-
-
-
-
